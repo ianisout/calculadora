d4d89be2acf16352a2ea4820564fa20882fe3db9
Nathalia Tescarollo <nathtescarollo@gmail.com>
2021-05-26 19:17:21 -0300

    Adicionando funções de multiplicação e calculadora

--- operacoes.js
+++ operacoes.js
@@ -2,10 +2,28 @@ function somarFelipe(a, b){
 
     return a + b
 };
-console.log(somarFelipe(5,8));
 
 function subtracaoAdriano(a, b){
     return a - b;
 }
 
-console.log(subtracaoAdriano(25, 17));
+function multiplicarNathalia(a, b) {
+	return a * b;
+}
+
+// function responsavel por chamar a operacao passada como parametro
+function calculadora(a, b, operacao) {
+	return operacao(a, b);
+};
+
+// definir nome da operação a ser testada e sua respectiva function
+const operacaoNome = "multiplicação";
+const operacaoFunc = multiplicarNathalia;
+
+const numA = 5;
+const numB = 8;
+
+console.log(`O resultado da ${operacaoNome} é ${calculadora(numA, numB, operacaoFunc)}`);
+
+// console.log(somarFelipe(5,8));
+// console.log(subtracaoAdriano(25, 17));
